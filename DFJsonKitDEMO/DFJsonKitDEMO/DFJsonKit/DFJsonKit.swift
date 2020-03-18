29033dc01bf39fadd1503f269a24cc5584dc755a
Fatm <569676974@qq.com>
2020-03-18 10:33:02 +0800

    update

--- DFJsonKitDEMO/DFJsonKitDEMO/DFJsonKit/DFJsonKit.swift
+++ DFJsonKitDEMO/DFJsonKitDEMO/DFJsonKit/DFJsonKit.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class DFJsonKit{
 
-    /// Json对象转Model
+    /// Json对象(数组/字典)转Model
     static func jsonToModel<T:Codable>(withJSONObject obj:Any?,modelType:T.Type)->T?{
         /*
         jsonObject->data->model
@@ -29,7 +29,7 @@ class DFJsonKit{
         }
     }
     
-    /// Json对象转String
+    /// Json对象(数组/字典)转String
     static func jsonToString(withJSONObject obj:Any?)->String?{
         /*
          jsonObject->data->String
@@ -41,6 +41,49 @@ class DFJsonKit{
             let jsonData = try JSONSerialization.data(withJSONObject: obj as Any, options:[])
             let string = String.init(data: jsonData, encoding: String.Encoding.utf8)
             return string
+        }catch{
+            print(error)
+            return nil
+        }
+    }
+    
+    /// Model转String
+    static func modelToString<T:Codable>(obj:T)->String?{
+        /*
+        object->data->String
+        */
+        do{
+            let data = try JSONEncoder().encode(obj)
+            let string = String.init(data: data, encoding: String.Encoding.utf8)
+            return string
+        }catch{
+            print(error)
+            return nil
+        }
+    }
+
+    /// String转Model
+    static func stringToModel<T:Codable>(string:String,modelType:T.Type)->T?{
+        /*
+         String->data->model
+         */
+        do{
+            let data = string.data(using: String.Encoding.utf8)!
+            let model = try JSONDecoder().decode(modelType, from: data)
+            return model
+        }catch{
+            print(error)
+            return nil
+        }
+    }
+    
+    /// Model转Json对象(数组/字典)
+    func modelTojson<T:Codable>(obj:T)->Any?{
+        do{
+            let data = try JSONEncoder().encode(obj)
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+            return json
         }catch{
             print(error)
             return nil
